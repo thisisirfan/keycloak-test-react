da8f3b85b45a12d90042ae0991dc93f8451ecc34
thisisirfan <irfansaddress@gmail.com>
2023-07-13 18:00:09 +0500

    logout issue fix

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,134 +1,224 @@
-import React, { ChangeEvent, useState } from 'react'
-import JsonTreeViewer from './JsonTreeViewer';
+import React, { ChangeEvent, useState } from "react";
+import JsonTreeViewer from "./JsonTreeViewer";
 interface loginDetails {
-    baseurl: string;
-    username: string;
-    password: string;
-    grant_type: string;
-    client_id: string;
-    scope: string;
+  baseurl: string;
+  username: string;
+  password: string;
+  grant_type: string;
+  client_id: string;
+  scope: string;
 }
-
 
 const LoginForm = () => {
-    const [formDetail, setFormDetail] = useState<loginDetails>({
-        baseurl: "",
-        username: "",
-        password: "",
-        grant_type: "password",
-        client_id: "nexi-app",
-        scope: "offline_access openid",
-    })
+  const [formDetail, setFormDetail] = useState<loginDetails>({
+    baseurl: "",
+    username: "",
+    password: "",
+    grant_type: "password",
+    client_id: "nexi-app",
+    scope: "offline_access openid",
+  });
 
-    const [apiResponse, setApiResponse] = useState<any>(null)
-    const [userInfo, setUserInfo] = useState<any>(null)
+  const API_URL: any = process.env.REACT_APP_API_URL;
 
-    const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
-        setFormDetail({
-            ...formDetail,
-            [ev.target.name]: ev.target.value
+  const [apiResponse, setApiResponse] = useState<any>(null);
+  const [userInfo, setUserInfo] = useState<any>(null);
 
-        })
-    }
+  const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setFormDetail({
+      ...formDetail,
+      [ev.target.name]: ev.target.value,
+    });
+  };
 
+  const handleSubmit = async () => {
+    if (
+      formDetail &&
+      formDetail.baseurl &&
+      formDetail.username &&
+      formDetail.password
+    ) {
+      try {
+        const { baseurl, ...otherprops } = formDetail;
+        console.log(
+          "🚀 ~ file: LoginForm.tsx:42 ~ handleSubmit ~ otherprops:",
+          otherprops
+        );
+        const formData = new URLSearchParams();
 
-    const handleSubmit = async () => {
-        if (formDetail && formDetail.baseurl && formDetail.username && formDetail.password) {
-            try {
-                const {
-                    baseurl,
-                    ...otherprops
-                } = formDetail
-                    console.log("🚀 ~ file: LoginForm.tsx:42 ~ handleSubmit ~ otherprops:", otherprops)
-                    const formData = new URLSearchParams();
-
-                    for (const [key, value] of Object.entries(otherprops)) {
-                      formData.append(key, value);
-                    }
-                const response: any = await fetch(baseurl.endsWith('/') ? baseurl + "token" : baseurl + "/token", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: formData.toString()
-                });
-
-                console.log("🚀 ~ file: LoginForm.tsx:47 ~ handleSubmit ~ response:", response)
-                const json = await response.json();
-                setApiResponse(json)
-
-            } catch (error) {
-                console.log('Error:', error);
-            }
+        for (const [key, value] of Object.entries(otherprops)) {
+          formData.append(key, value);
         }
-    }
+        const response: any = await fetch(
+          baseurl.endsWith("/") ? baseurl + "token" : baseurl + "/token",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: formData.toString(),
+          }
+        );
 
-    const handleUserInfo = async () => {
-        try {
-            const { baseurl } = formDetail
-            const response = await fetch(baseurl.endsWith('/') ? baseurl + "userinfo" : baseurl + "/userinfo", {
-                headers: {
-                    'Authorization': "Bearer " + apiResponse?.access_token
-                }
-            });
-            console.log("🚀 ~ file: LoginForm.tsx:75 ~ response ~ response:", response)
-            const json = await response.json();
-            console.log("🚀 ~ file: LoginForm.tsx:77 ~ handleUserInfo ~ json:", json)
-            setUserInfo(json)
-        } catch (error) {
-            console.log('Error:', error);
+        console.log(
+          "🚀 ~ file: LoginForm.tsx:47 ~ handleSubmit ~ response:",
+          response
+        );
+        const json = await response.json();
+        setApiResponse(json);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    }
+  };
+
+  const handleUserInfo = async () => {
+    try {
+      const { baseurl } = formDetail;
+      const response = await fetch(
+        baseurl.endsWith("/") ? baseurl + "userinfo" : baseurl + "/userinfo",
+        {
+          headers: {
+            Authorization: "Bearer " + apiResponse?.access_token,
+          },
         }
+      );
+      console.log(
+        "🚀 ~ file: LoginForm.tsx:75 ~ response ~ response:",
+        response
+      );
+      const json = await response.json();
+      console.log("🚀 ~ file: LoginForm.tsx:77 ~ handleUserInfo ~ json:", json);
+      setUserInfo(json);
+    } catch (error) {
+      console.log("Error:", error);
     }
+  };
 
-    const handleLogout = async () => {
-        try {
-            const { baseurl } = formDetail
-            const response = await fetch(baseurl.endsWith('/') ? baseurl + "logout" : baseurl + "/logout", {
-                headers: {
-                    'Authorization': 'Bearer your_token_here' // Replace with your actual token
-                }
-            });
-            setUserInfo(null)
-            setApiResponse(null)
-        } catch (error) {
-            console.log('Error:', error);
+  const handleProductsInfo = async () => {
+    try {
+      const response = await fetch(API_URL + "/users/devices", {
+        headers: {
+          Authorization: "Bearer " + apiResponse?.access_token,
+        },
+      });
+      const json = await response.json();
+      console.log(
+        "🚀 ~ file: LoginForm.tsx:94 ~ handleProductsInfo ~ json:",
+        json
+      );
+      // setUserInfo(json)
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: LoginForm.tsx:97 ~ handleProductsInfo ~ error:",
+        error
+      );
+    }
+  };
+
+  const handleLogout = async () => {
+    const browserBaseUrl = window.location.origin;
+    try {
+      const { baseurl } = formDetail;
+      const response = await fetch(
+        baseurl.endsWith("/") ? baseurl + "logout" : baseurl + `/logout?id_token_hint=${apiResponse?.id_token}`,
+        {
+          headers: {
+            Authorization: "Bearer your_token_here", // Replace with your actual token
+          },
         }
+      );
+      setUserInfo(null);
+      setApiResponse(null);
+    } catch (error) {
+      console.log("Error:", error);
     }
+  };
 
-
-    return (
-        <div className="container">
-            <div className="row justify-content-center mt-5 ">
-                <div className="col-md-8 border p-4">
-                    <h2 className="text-center">Login Form</h2>
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="username">Base URL</label>
-                            <input onChange={(e) => handleChange(e)} name="baseurl" type="text" className="form-control" id="baseurl" placeholder="Enter your base url" />
-                            <small>i.e: <i>https://idp.stg.nexi-international.com/realms/nexi/protocol/openid-connect/</i></small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <input onChange={(e) => handleChange(e)} type="text" className="form-control" id="username" name="username" placeholder="Enter your username" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input onChange={(e) => handleChange(e)} type="password" className="form-control" id="password" name="password" placeholder="Enter your password" />
-                        </div>
-                        <button type="button" onClick={() => handleSubmit()} className="btn btn-primary mr-2">Login</button>
-                        {apiResponse && (
-                            <>
-                                <button type="button" onClick={() => handleUserInfo()} className="btn btn-success mr-2">User Info</button>
-                                <button type="button" onClick={() => handleLogout()} className="btn btn-danger mr-2">Logout</button>
-                            </>
-                        )}
-                    </form>
-                    <JsonTreeViewer data={userInfo} title='User Info'/>
-                    <JsonTreeViewer data={apiResponse} title='Api Response'/>
-                </div>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5 ">
+        <div className="col-md-8 border p-4">
+          <h2 className="text-center">Login Form</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">Base URL</label>
+              <input
+                onChange={(e) => handleChange(e)}
+                name="baseurl"
+                type="text"
+                className="form-control"
+                id="baseurl"
+                placeholder="Enter your base url"
+              />
+              <small>
+                i.e:{" "}
+                <i>
+                  https://idp.stg.nexi-international.com/realms/nexi/protocol/openid-connect/
+                </i>
+              </small>
             </div>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                onChange={(e) => handleChange(e)}
+                type="text"
+                className="form-control"
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                onChange={(e) => handleChange(e)}
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => handleSubmit()}
+              className="btn btn-primary mr-2"
+            >
+              Login
+            </button>
+            {apiResponse && (
+              <>
+                <button
+                  type="button"
+                  onClick={() => handleUserInfo()}
+                  className="btn btn-success mr-2"
+                >
+                  User Info
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleProductsInfo()}
+                  className="btn btn-success mr-2"
+                >
+                  Products
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleLogout()}
+                  className="btn btn-danger mr-2"
+                >
+                  Logout
+                </button>
+              </>
+            )}
+          </form>
+          <JsonTreeViewer data={userInfo} title="User Info" />
+          <JsonTreeViewer data={apiResponse} title="Api Response" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
